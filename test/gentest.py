877ebf4dcf75a229ce3e985ba0b948eb7d0c05b5
Shawn Anastasio <shawn@anastas.io>
2020-11-20 16:31:44 -0600

    test/gentest: Add new test suite for ALU insns
    New test suite tests ALU instructions for correct results
    across a variety of operand sizes. Currently only SUB.

--- test/gentest.py
+++ test/gentest.py
@@ -24,7 +24,102 @@ ASM_EPILOG = \
     syscall
 """
 
-FlagTestCase = namedtuple("FlagTestCase", ["width", "reg1_str", "reg2_str", "imm1_str", "imm2_str", "insn_str", "jmp_good", "jmp_bad"])
+def flag_test_case_genstrs(test, n, f):
+    width_str = width_to_cast(test.width)
+    f.write("    TEST_{}_STR_PASS: .ascii \"PASS: ({} {}{}, {}{}) -> {}\\n\"\n".format(n, test.insn_str, width_str, test.imm1_str, width_str, test.imm2_str, test.jmp_good))
+    f.write("    TEST_{}_STR_PASS_LEN = . - TEST_{}_STR_PASS\n".format(n, n))
+    f.write("    TEST_{}_STR_FAIL: .ascii \"FAIL: ({} {}{}, {}{}) -> {}\\n\"\n".format(n, test.insn_str, width_str, test.imm1_str, width_str, test.imm2_str, test.jmp_bad))
+    f.write("    TEST_{}_STR_FAIL_LEN = . - TEST_{}_STR_FAIL\n".format(n, n))
+    f.write("    TEST_{}_STR_UNREACHABLE: .ascii \"FAIL: ({} {}{}, {}{}) -> UNREACHABLE!\\n\"\n".format(n, test.insn_str, width_str, test.imm1_str, width_str, test.imm2_str))
+    f.write("    TEST_{}_STR_UNREACHABLE_LEN = . - TEST_{}_STR_UNREACHABLE\n".format(n, n))
+    f.write("\n")
+
+def flag_test_case_gentest(test, n, f):
+    f.write("{}: # Test {}\n".format(n * 10, n))
+    f.write("    mov {}, OFFSET {}\n".format(test.reg1_str, test.imm1_str))
+    if test.reg2_str == "":
+        f.write("    {} {}, OFFSET {}\n".format(test.insn_str, test.reg1_str, test.imm2_str))
+    else:
+        f.write("    mov {}, OFFSET {}\n".format(test.reg2_str, test.imm2_str))
+        f.write("    {} {}, {}\n".format(test.insn_str, test.reg1_str, test.reg2_str))
+    f.write("    {} 2f # Bad\n".format(test.jmp_bad))
+    f.write("    {} 1f # Good\n".format(test.jmp_good))
+
+    # Unreachable
+    f.write("    print TEST_{}_STR_UNREACHABLE, TEST_{}_STR_UNREACHABLE_LEN\n".format(n, n))
+    f.write("    jmp 3f\n")
+
+    # Good
+    f.write("1: # Test {} PASS\n".format(n))
+    f.write("    print TEST_{}_STR_PASS, TEST_{}_STR_PASS_LEN\n".format(n, n))
+    f.write("    jmp 3f\n")
+
+    # Fail
+    f.write("2: # Test {} FAIL\n".format(n))
+    f.write("    print TEST_{}_STR_FAIL, TEST_{}_STR_FAIL_LEN\n".format(n, n))
+    f.write("3:\n\n")
+
+
+FlagTestCase = namedtuple("FlagTestCase", ["width", "reg1_str", "reg2_str", "imm1_str", "imm2_str",
+                                           "insn_str", "jmp_good", "jmp_bad", "f_genstrs", "f_gentest"],
+                                          defaults=(None,None,None,None,None,None,None,None,flag_test_case_genstrs,flag_test_case_gentest))
+
+
+def alu_test_case_genstrs(test, n, f):
+    width_str = width_to_cast(test.width)
+    f.write("    TEST_{}_STR_PASS: .ascii \"PASS: ({} {}{}, {}{}) -> {} == {}\\n\"\n".format(n, test.insn_str, width_str, test.imm1_str, width_str, test.imm2_str, test.reg1_str, test.res_str))
+    f.write("    TEST_{}_STR_PASS_LEN = . - TEST_{}_STR_PASS\n".format(n, n))
+    f.write("    TEST_{}_STR_FAIL: .ascii \"FAIL: ({} {}{}, {}{}) -> {} != {}\\n\"\n".format(n, test.insn_str, width_str, test.imm1_str, width_str, test.imm2_str, test.reg1_str, test.res_str))
+    f.write("    TEST_{}_STR_FAIL_LEN = . - TEST_{}_STR_FAIL\n".format(n, n))
+
+def alu_test_case_gentest(test, n, f):
+    f.write("{}: # Test {}\n".format(n * 10, n))
+    f.write("    mov {}, OFFSET {}\n".format(test.reg1_str, test.imm1_str))
+    if test.reg2_str == "":
+        f.write("    {} {}, OFFSET {}\n".format(test.insn_str, test.reg1_str, test.imm2_str))
+    else:
+        f.write("    mov {}, OFFSET {}\n".format(test.reg2_str, test.imm2_str))
+        f.write("    {} {}, {}\n".format(test.insn_str, test.reg1_str, test.reg2_str))
+    f.write("    cmp {}, OFFSET {}\n".format(test.reg1_str, test.res_str))
+    f.write("    je 1f\n")
+
+    # Fail
+    f.write("# Test {} FAIL\n".format(n))
+    f.write("    print TEST_{}_STR_FAIL, TEST_{}_STR_FAIL_LEN\n".format(n, n))
+    f.write("    jmp 2f\n")
+
+    # Success
+    f.write("1: # Test {} PASS\n".format(n))
+    f.write("    print TEST_{}_STR_PASS, TEST_{}_STR_PASS_LEN\n".format(n, n))
+    f.write("2:\n\n")
+
+
+AluTestCase = namedtuple("AluTestCase", ["width", "insn_str", "reg1_str", "reg2_str", "imm1_str", "imm2_str",
+                                         "res_str", "f_genstrs", "f_gentest"],
+                                        defaults=(None,None,None,None,None,None,None,alu_test_case_genstrs,alu_test_case_gentest))
+
+ALU_TESTS = [
+    AluTestCase(64, "sub", "rax", "rbx", "100", "99", "1"),
+    AluTestCase(64, "sub", "rax", "rbx", "0", "1", "-1"),
+    AluTestCase(64, "sub", "rax", "rbx", "1", "1", "0"),
+
+    AluTestCase(32, "sub", "eax", "ebx", "100", "99", "1"),
+    AluTestCase(32, "sub", "eax", "ebx", "0", "1", "-1"),
+    AluTestCase(32, "sub", "eax", "ebx", "1", "1", "0"),
+
+    AluTestCase(16, "sub", "ax", "bx", "100", "99", "1"),
+    AluTestCase(16, "sub", "ax", "bx", "0", "1", "-1"),
+    AluTestCase(16, "sub", "ax", "bx", "1", "1", "0"),
+
+    AluTestCase(8, "sub", "ah", "bh", "100", "99", "1"),
+    AluTestCase(8, "sub", "ah", "bh", "0", "1", "-1"),
+    AluTestCase(8, "sub", "ah", "bh", "1", "1", "0"),
+
+    AluTestCase(8, "sub", "al", "bl", "100", "99", "1"),
+    AluTestCase(8, "sub", "al", "bl", "0", "1", "-1"),
+    AluTestCase(8, "sub", "al", "bl", "1", "1", "0"),
+
+]
 
 CARRY_TESTS = [
     # SUB (cmp)
@@ -98,7 +193,8 @@ OVERFLOW_TESTS = [
 
 SUITES = {
     "CF" : CARRY_TESTS,
-    "OF" : OVERFLOW_TESTS
+    "OF" : OVERFLOW_TESTS,
+    "ALU" : ALU_TESTS,
 }
 
 def width_to_cast(width):
@@ -113,14 +209,7 @@ def generate_tests(suites, f):
     n = 0
     for suite in suites:
         for test in suite:
-            width_str = width_to_cast(test.width)
-            f.write("    TEST_{}_STR_PASS: .ascii \"PASS: ({} {}{}, {}{}) -> {}\\n\"\n".format(n, test.insn_str, width_str, test.imm1_str, width_str, test.imm2_str, test.jmp_good))
-            f.write("    TEST_{}_STR_PASS_LEN = . - TEST_{}_STR_PASS\n".format(n, n))
-            f.write("    TEST_{}_STR_FAIL: .ascii \"FAIL: ({} {}{}, {}{}) -> {}\\n\"\n".format(n, test.insn_str, width_str, test.imm1_str, width_str, test.imm2_str, test.jmp_bad))
-            f.write("    TEST_{}_STR_FAIL_LEN = . - TEST_{}_STR_FAIL\n".format(n, n))
-            f.write("    TEST_{}_STR_UNREACHABLE: .ascii \"FAIL: ({} {}{}, {}{}) -> UNREACHABLE!\\n\"\n".format(n, test.insn_str, width_str, test.imm1_str, width_str, test.imm2_str))
-            f.write("    TEST_{}_STR_UNREACHABLE_LEN = . - TEST_{}_STR_UNREACHABLE\n".format(n, n))
-            f.write("\n")
+            test.f_genstrs(test, n, f)
             n = n + 1
 
     f.write(".text\n")
@@ -131,32 +220,7 @@ def generate_tests(suites, f):
     n = 0
     for suite in suites:
         for test in suite:
-            assert test.__class__ is FlagTestCase # TODO support other types of tests
-
-            f.write("{}: # Test {}\n".format(n * 10, n))
-            f.write("    mov {}, OFFSET {}\n".format(test.reg1_str, test.imm1_str))
-            if test.reg2_str == "":
-                f.write("    {} {}, OFFSET {}\n".format(test.insn_str, test.reg1_str, test.imm2_str))
-            else:
-                f.write("    mov {}, OFFSET {}\n".format(test.reg2_str, test.imm2_str))
-                f.write("    {} {}, {}\n".format(test.insn_str, test.reg1_str, test.reg2_str))
-            f.write("    {} 2f # Bad\n".format(test.jmp_bad))
-            f.write("    {} 1f # Good\n".format(test.jmp_good))
-
-            # Unreachable
-            f.write("    print TEST_{}_STR_UNREACHABLE, TEST_{}_STR_UNREACHABLE_LEN\n".format(n, n))
-            f.write("    jmp 3f\n")
-
-            # Good
-            f.write("1: # Test {} PASS\n".format(n))
-            f.write("    print TEST_{}_STR_PASS, TEST_{}_STR_PASS_LEN\n".format(n, n))
-            f.write("    jmp 3f\n")
-
-            # Fail
-            f.write("2: # Test {} FAIL\n".format(n))
-            f.write("    print TEST_{}_STR_FAIL, TEST_{}_STR_FAIL_LEN\n".format(n, n))
-            f.write("3:\n\n")
-
+            test.f_gentest(test, n, f)
             n = n + 1
 
 
