shawnanastasio/retrec

commit 1d50e7622ab576a3c7191a55461ecacc42134563

Implement support for SSE MOVD (32-bit) load/store
MOVD is the 32-bit SSE instruction that provides loads, stores, and
xmm<->mmx<->gpr moves. This commit implements support for the former
2 forms in the x86_64 llir lifter and the ppc64le codegen backend.

src/arch/ppc64le/codegen/codegen_ppc64le.cpp
@@ -3067,6 +3067,18 @@ void codegen_ppc64le<TargetTraitsX86_64>::macro$loadstore(gen_context &ctx, cons
     auto lbax  = [&](auto a, auto b, auto c) { ctx.assembler->lbzx(a, b, c); ctx.assembler->extsb(a, a); };
     auto lbaux = [&](auto a, auto b, auto c) { ctx.assembler->lbzux(a, b, c); ctx.assembler->extsb(a, a); };
 
+    auto vsx_loadx_lowlow32 = [&](auto a, auto b, auto c) {
+        auto tmp = ctx.reg_allocator().allocate_gpr();
+        ctx.assembler->lwzx(tmp.gpr(), b, c);
+        ctx.assembler->mtvsrdd(a, 0, tmp.gpr());
+    };
+
+    auto vsx_storex_lowlow32 = [&](auto a, auto b, auto c) {
+        auto tmp = ctx.reg_allocator().allocate_gpr();
+        ctx.assembler->mfvsrld(tmp.gpr(), a);
+        ctx.assembler->stwx(tmp.gpr(), b, c);
+    };
+
 // Helpers to call the appropriate loadstore op depending on whether `update` is set or not
 #define LOAD_DISP(op, ...) ((update == llir::MemOp::Update::PRE) ? op ## u(__VA_ARGS__) : op(__VA_ARGS__))
 #define LOAD_INDEXED(op, ...) ((update == llir::MemOp::Update::PRE) ? op ## ux(__VA_ARGS__) : op ## x(__VA_ARGS__))
@@ -3156,15 +3168,18 @@ void codegen_ppc64le<TargetTraitsX86_64>::macro$loadstore(gen_context &ctx, cons
 
             case llir::LoadStore::Op::VECTOR_LOAD:
             {
+                // We don't need to worry about source memory alignment since the kernel will automatically
+                // correct unaligned accesses for us. This is probably slower than manually emitting an unaligned
+                // load ourselves, but it's easier so it works for now.
                 assert(extension == llir::Extension::NONE);
                 assert(update == llir::MemOp::Update::NONE);
+                assert(reg.zero_others);
                 switch (reg.mask) {
                     case llir::Register::Mask::Vector128Full: ctx.assembler->lxv(r.vsr(), ra, disp); break;
                     case llir::Register::Mask::Vector128High64:
                     case llir::Register::Mask::Vector128Low64:
-                    case llir::Register::Mask::Vector128Low32:
-                        TODO();
-
+                    case llir::Register::Mask::Vector128LowLow32:
+                        // Non-128-bit loads don't have displacement forms
                     default:
                         ASSERT_NOT_REACHED();
                 }
@@ -3180,9 +3195,8 @@ void codegen_ppc64le<TargetTraitsX86_64>::macro$loadstore(gen_context &ctx, cons
                     case llir::Register::Mask::Vector128Full: ctx.assembler->stxv(r.vsr(), ra, disp); break;
                     case llir::Register::Mask::Vector128High64:
                     case llir::Register::Mask::Vector128Low64:
-                    case llir::Register::Mask::Vector128Low32:
-                        TODO();
-
+                    case llir::Register::Mask::Vector128LowLow32:
+                        // Non-128-bit stores don't have displacement forms
                     default:
                         ASSERT_NOT_REACHED();
                 }
@@ -3250,6 +3264,7 @@ void codegen_ppc64le<TargetTraitsX86_64>::macro$loadstore(gen_context &ctx, cons
                         ASSERT_NOT_REACHED();
                 }
 
+                assert(!reg.zero_others);
                 break;
             }
 
@@ -3259,9 +3274,35 @@ void codegen_ppc64le<TargetTraitsX86_64>::macro$loadstore(gen_context &ctx, cons
                 break;
 
             case llir::LoadStore::Op::VECTOR_LOAD:
-                TODO();
+                assert(extension == llir::Extension::NONE);
+                assert(update == llir::MemOp::Update::NONE);
+                assert(reg.zero_others);
+                switch (reg.mask) {
+                    case llir::Register::Mask::Vector128Full: ctx.assembler->lxvx(r.vsr(), ra, rb); break;
+                    case llir::Register::Mask::Vector128High64: TODO(); break;
+                    case llir::Register::Mask::Vector128Low64: TODO(); break;
+                    case llir::Register::Mask::Vector128LowLow32: vsx_loadx_lowlow32(r.vsr(), ra, rb); break;
+
+                    default:
+                        ASSERT_NOT_REACHED();
+                }
+
+                break;
+
             case llir::LoadStore::Op::VECTOR_STORE:
-                TODO();
+                assert(extension == llir::Extension::NONE);
+                assert(update == llir::MemOp::Update::NONE);
+                switch (reg.mask) {
+                    case llir::Register::Mask::Vector128Full: ctx.assembler->stxvx(r.vsr(), ra, rb); break;
+                    case llir::Register::Mask::Vector128High64: TODO();
+                    case llir::Register::Mask::Vector128Low64: TODO();
+                    case llir::Register::Mask::Vector128LowLow32: vsx_storex_lowlow32(r.vsr(), ra, rb); break;
+
+                    default:
+                        ASSERT_NOT_REACHED();
+                }
+
+                break;
 
             default:
                 ASSERT_NOT_REACHED();
@@ -3314,7 +3355,15 @@ void codegen_ppc64le<TargetTraitsX86_64>::macro$loadstore(gen_context &ctx, cons
 
             case llir::LoadStore::Op::VECTOR_LOAD:
             case llir::LoadStore::Op::VECTOR_STORE:
-                disp_fits = assembler::fits_in_mask(disp, 0xFFF0U);
+                // Only certain VSX Loads/Stores support immediate displacements
+                switch (reg.mask) {
+                    case llir::Register::Mask::Vector128Full:
+                        disp_fits = assembler::fits_in_mask(disp, 0xFFF0U);
+                        break;
+
+                    default:
+                        disp_fits = false;
+                }
                 break;
 
             default:

src/arch/ppc64le/codegen/assembler.h
@@ -1010,6 +1010,35 @@ public:
         }, ra, rs, rb, modify_cr);
     }
 
+    // 3.3.16 Move To/From Vector-Scalar Register Instructions
+#define NORMALIZE_VSX_REGISTERS(...) FOR_EACH(NORMALIZE_VSX_REGISTER, ##__VA_ARGS__)
+#define NORMALIZE_VSX_REGISTER(reg) \
+    bool reg ## _is_upper = ((reg) >= 32); decltype((reg)) reg ## _n = ((reg) >= 32) ? ((reg) - 32) : (reg);
+
+    void mfvsrd(uint8_t xs, uint8_t ra) {
+        ASM_LOG("Emitting mfvsrd r%u, vs%u\n", ra, xs);
+        EMIT_INSN(Operation::MFVSRD, [=] {
+            NORMALIZE_VSX_REGISTERS(xs);
+            return self->x_form(31, xs_n, ra, 0, 51, xs_is_upper);
+        }, ra, xs);
+    }
+
+    void mfvsrld(uint8_t xs, uint8_t ra) {
+        ASM_LOG("Emitting mfvsrld r%u, vs%u\n", ra, xs);
+        EMIT_INSN(Operation::MFVSRLD, [=] {
+            NORMALIZE_VSX_REGISTERS(xs);
+            return self->x_form(31, xs_n, ra, 0, 307, xs_is_upper);
+        }, ra, xs);
+    }
+
+    void mtvsrdd(uint8_t xt, uint8_t ra, uint8_t rb) {
+        ASM_LOG("Emitting mtvsrdd vs%u, r%u, r%u\n", xt, ra, rb);
+        EMIT_INSN(Operation::MTVSRDD, [=] {
+            NORMALIZE_VSX_REGISTERS(xt);
+            return self->x_form(31, xt_n, ra, rb, 435, xt_is_upper);
+        }, xt, ra, rb);
+    }
+
     // 3.3.17 Move To/From System Register Instructions
     void mtspr(SPR spr, uint8_t rs) {
         ASM_LOG("Emitting mtspr r%u, %u\n", rs, (uint16_t)spr);
@@ -1072,9 +1101,15 @@ public:
     }
 
     // 7.6.3 VSX Instructions
-#define NORMALIZE_VSX_REGISTERS(...) FOR_EACH(NORMALIZE_VSX_REGISTER, ##__VA_ARGS__)
-#define NORMALIZE_VSX_REGISTER(reg) \
-    bool reg ## _is_upper = ((reg) >= 32); decltype((reg)) reg ## _n = ((reg) >= 32) ? ((reg) - 32) : (reg);
+
+    // VSX Load/Store
+    void lxsiwzx(uint8_t xt, uint8_t ra, uint8_t rb) {
+        ASM_LOG("Emitting lxsiwzx vs%u, r%u, r%u\n", xt, ra, rb);
+        EMIT_INSN(Operation::LXSIWZX, [=] {
+            NORMALIZE_VSX_REGISTERS(xt);
+            return self->x_form(31, xt_n, ra, rb, 12, xt_is_upper);
+        }, xt, ra, rb);
+    }
 
     void lxv(uint8_t xt, uint8_t ra, int16_t dq) {
         ASM_LOG("Emitting lxv vs%u, %d(r%u)\n", xt, dq, ra);

src/arch/ppc64le/codegen/codegen_types.h
@@ -84,6 +84,7 @@
     x(LWZU, &assembler::lwzu) \
     x(LWZUX, &assembler::lwzux) \
     x(LWZX, &assembler::lwzx) \
+    x(LXSIWZX, &assembler::lxsiwzx) \
     x(LXV, &assembler::lxv) \
     x(LXVX, &assembler::lxvx) \
     x(MCRF, &assembler::mcrf) \
@@ -91,9 +92,12 @@
     x(MFCR, &assembler::mfcr) \
     x(MFOCRF, &assembler::mfocrf) \
     x(MFSPR, &assembler::mfspr) \
+    x(MFVSRD, &assembler::mfvsrd) \
+    x(MFVSRLD, &assembler::mfvsrld) \
     x(MTCRF, &assembler::mtcrf) \
     x(MTOCRF, &assembler::mtocrf) \
     x(MTSPR, &assembler::mtspr) \
+    x(MTVSRDD, &assembler::mtvsrdd) \
     x(MULHD, &assembler::mulhd) \
     x(MULHDU, &assembler::mulhdu) \
     x(MULHW, &assembler::mulhw) \

src/arch/x86_64/llir/llir_lifter_x86_64.cpp
@@ -54,6 +54,13 @@ status_code llir_lifter_x86_64::lift(cs_insn *insn, std::vector<llir::Insn> &out
     llir::Extension extension = llir::Extension::NONE;
     llir::Register::Mask op_mask;
     llir::Register::TypeHint op_type_hint;
+    enum class ZeroOthersMode {
+        DONT_TOUCH,
+        TRUE,
+        FALSE
+    };
+    ZeroOthersMode zero_others_move_reg {};
+    ZeroOthersMode zero_others_load {};
     bool last_access_hint { false };
     bool require_alignment;
 
@@ -489,17 +496,27 @@ status_code llir_lifter_x86_64::lift(cs_insn *insn, std::vector<llir::Insn> &out
             }
             break;
 
-        case X86_INS_MOVAPD:   require_alignment = true; op_mask = Mask::Vector128Full; op_type_hint = TypeHint::DOUBLE; goto mov128_common;
-        case X86_INS_MOVAPS:   require_alignment = true; op_mask = Mask::Vector128Full; op_type_hint = TypeHint::FLOAT; goto mov128_common;
-        case X86_INS_MOVDQA:   require_alignment = true; op_mask = Mask::Vector128Full; op_type_hint = TypeHint::INT; goto mov128_common;
-        case X86_INS_MOVUPD:   require_alignment = false; op_mask = Mask::Vector128Full; op_type_hint = TypeHint::DOUBLE; goto mov128_common;
-        case X86_INS_MOVUPS:   require_alignment = false; op_mask = Mask::Vector128Full; op_type_hint = TypeHint::FLOAT; goto mov128_common;
-        case X86_INS_MOVDQU:   require_alignment = false; op_mask = Mask::Vector128Full; op_type_hint = TypeHint::INT; goto mov128_common;
-        case X86_INS_MOVNTDQA: require_alignment = true; op_mask = Mask::Vector128Full; op_type_hint = TypeHint::INT; last_access_hint = true; goto mov128_common;
-        case X86_INS_MOVNTPD:  require_alignment = true; op_mask = Mask::Vector128Full; op_type_hint = TypeHint::DOUBLE; last_access_hint = true; goto mov128_common;
-        case X86_INS_MOVNTPS:  require_alignment = true; op_mask = Mask::Vector128Full; op_type_hint = TypeHint::FLOAT; last_access_hint = true; goto mov128_common;
-        case X86_INS_MOVNTDQ:  require_alignment = true; op_mask = Mask::Vector128Full; op_type_hint = TypeHint::INT; last_access_hint = true; goto mov128_common;
-        mov128_common:
+        // <128-bit Legacy SSE MOVs
+        case X86_INS_MOVD:
+            require_alignment = 0;
+            zero_others_move_reg = ZeroOthersMode::TRUE;
+            zero_others_load = ZeroOthersMode::TRUE;
+            op_mask = Mask::Vector128LowLow32;
+            op_type_hint = TypeHint::INT;
+            goto mov_legacy_sse_common;
+
+        // 128-bit Legacy SSE MOVs
+        case X86_INS_MOVAPD:   require_alignment = 1; op_mask = Mask::Vector128Full; op_type_hint = TypeHint::DOUBLE; goto mov_legacy_sse_common;
+        case X86_INS_MOVAPS:   require_alignment = 1; op_mask = Mask::Vector128Full; op_type_hint = TypeHint::FLOAT; goto mov_legacy_sse_common;
+        case X86_INS_MOVDQA:   require_alignment = 1; op_mask = Mask::Vector128Full; op_type_hint = TypeHint::INT; goto mov_legacy_sse_common;
+        case X86_INS_MOVDQU:   require_alignment = 0; op_mask = Mask::Vector128Full; op_type_hint = TypeHint::INT; goto mov_legacy_sse_common;
+        case X86_INS_MOVNTDQ:  require_alignment = 1; op_mask = Mask::Vector128Full; op_type_hint = TypeHint::INT; last_access_hint = true; goto mov_legacy_sse_common;
+        case X86_INS_MOVNTDQA: require_alignment = 1; op_mask = Mask::Vector128Full; op_type_hint = TypeHint::INT; last_access_hint = true; goto mov_legacy_sse_common;
+        case X86_INS_MOVNTPD:  require_alignment = 1; op_mask = Mask::Vector128Full; op_type_hint = TypeHint::DOUBLE; last_access_hint = true; goto mov_legacy_sse_common;
+        case X86_INS_MOVNTPS:  require_alignment = 1; op_mask = Mask::Vector128Full; op_type_hint = TypeHint::FLOAT; last_access_hint = true; goto mov_legacy_sse_common;
+        case X86_INS_MOVUPD:   require_alignment = 0; op_mask = Mask::Vector128Full; op_type_hint = TypeHint::DOUBLE; goto mov_legacy_sse_common;
+        case X86_INS_MOVUPS:   require_alignment = 0; op_mask = Mask::Vector128Full; op_type_hint = TypeHint::FLOAT; goto mov_legacy_sse_common;
+        mov_legacy_sse_common:
             assert(detail->x86.op_count == 2);
             llinsn.dest_cnt = 1;
             llinsn.src_cnt = 1;
@@ -521,6 +538,8 @@ status_code llir_lifter_x86_64::lift(cs_insn *insn, std::vector<llir::Insn> &out
                 fill_operand(detail->x86.operands[0], llinsn.dest[0]);
                 llinsn.dest[0].reg().type_hint = op_type_hint;
                 llinsn.dest[0].reg().mask = op_mask;
+                if (zero_others_load != ZeroOthersMode::DONT_TOUCH)
+                    llinsn.dest[0].reg().zero_others = zero_others_load == ZeroOthersMode::TRUE;
                 fill_operand(detail->x86.operands[1], llinsn.src[0]);
             } else if (detail->x86.operands[0].type == X86_OP_REG && detail->x86.operands[1].type == X86_OP_REG) {
                 // mov xmmN, xmmN, - Move Register
@@ -528,6 +547,8 @@ status_code llir_lifter_x86_64::lift(cs_insn *insn, std::vector<llir::Insn> &out
                 fill_operand(detail->x86.operands[0], llinsn.dest[0]);
                 llinsn.dest[0].reg().type_hint = op_type_hint;
                 llinsn.dest[0].reg().mask = op_mask;
+                if (zero_others_move_reg != ZeroOthersMode::DONT_TOUCH)
+                    llinsn.dest[0].reg().zero_others = zero_others_move_reg == ZeroOthersMode::TRUE;
                 fill_operand(detail->x86.operands[1], llinsn.src[0]);
                 llinsn.src[0].reg().type_hint = op_type_hint;
                 llinsn.src[0].reg().mask = op_mask;

src/llir.h
@@ -59,10 +59,10 @@ struct Register {
         Full64,      // 63 downto 0
 
         // 128-bit vector masks
-        Vector128Full,   // 127 downto 0
-        Vector128High64, // 127 downto 64
-        Vector128Low64,  // 63 downto 0
-        Vector128Low32,  // 31 downto 0
+        Vector128Full,      // 127 downto 0
+        Vector128High64,    // 127 downto 64
+        Vector128Low64,     // 63 downto 0
+        Vector128LowLow32,  // 31 downto 0
 
         Special
     } mask {};
@@ -490,7 +490,7 @@ inline std::string to_string(const Register::Mask &mask) {
         case Register::Mask::Vector128Full: return "Vector128Full";
         case Register::Mask::Vector128High64: return "Vector128High64";
         case Register::Mask::Vector128Low64: return "Vector128Low64";
-        case Register::Mask::Vector128Low32: return "Vector128Low32";
+        case Register::Mask::Vector128LowLow32: return "Vector128LowLow32";
         case Register::Mask::Special: return "Special";
     }
     ASSERT_NOT_REACHED();

commit a76ffe37c7e5f973d2dffb31b31aea60ec882a25

arch/ppc64le/codegen: Fix regression in macro$loadstore
Commit 1d50e762 introduced an erroneous assertion for !zero_others in a
STORE path of macro$loadstore despite the condition being perfectly
valid. This resulted in a few tests failing, including pushpop.S.

src/arch/ppc64le/codegen/codegen_ppc64le.cpp
@@ -3264,7 +3264,6 @@ void codegen_ppc64le<TargetTraitsX86_64>::macro$loadstore(gen_context &ctx, cons
                         ASSERT_NOT_REACHED();
                 }
 
-                assert(!reg.zero_others);
                 break;
             }
 